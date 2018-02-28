a36d624a88ab238bbc2efc4e4be0f28d1ac8b651
tomiborza <borzatamas00@gmail.com>
2018-02-28 10:05:25 +0100

    csillagdoboz

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 echo "<center>";
 function greeting(var name){
 	for(i=0;i<79;i++)echo "*";echo "</br>";
+	for(i=0;i<79;i++)if(i==0||i==79)echo "*";echo "</br>";
 	if(name==NULL){
 		echo "* Happy easter my friend *";
 	}else{
 		echo "* Happy easter "+name+" *";
 	}
+	for(i=0;i<79;i++)if(i==0||i==79)echo "*";echo "</br>";
 	for(i=0;i<79;i++)echo "*";echo "</br>";
 
 }
