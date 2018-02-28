2030b5296158807665a7657fdedb8c32d594529a
tomiborza <borzatamas00@gmail.com>
2018-02-28 10:53:41 +0100

    16

--- index.php
+++ index.php
@@ -1,19 +1,34 @@
 <?php
 echo "<center>";
-function greeting(var name){
-	var i;
-	for(i=0;i<79;i++)echo "#";echo "</br>";
-	for(i=0;i<79;i++)echo "#";echo "</br>";
-	for(i=0;i<79;i++)if(i==0||i==79)echo "##";echo "</br>";
-
-	if(name==NULL){
-		echo "##   Happy easter my friend   ##";
-	}else{
-		echo "##   Happy easter "+name+"   ##";
+function keret(){
+	for($i=0;$i<79;$i++)echo "#";echo "</br>";
+}
+function greeting($name){
+	$mf="Happy easter my friend";
+	$nm="Happy easter "+name+"";
+	$l=79
+	if(strlen($mf)>80){
+		$com=strlen($mf)-80;
+		$l=$l+$com;
 	}
-	for(i=0;i<79;i++)if(i==0||i==79)echo "##";echo "</br>";
-	for(i=0;i<79;i++)echo "#";echo "</br>";
-	for(i=0;i<79;i++)echo "#";echo "</br>";
+	$com=0;
+	if(strlen($mn)>80){
+		$com=strlen($mf)-80;
+		$l=$l+$com;
+	}
+	keret();
+	keret();
+	for($i=0;$i<$l;$i++)if($i==0||$i==79)echo "##";echo "</br>";
+
+	if($name==NULL){
+		echo "##   "+$mf+"   ##";
+	}else{
+		echo "##   "+$mn+"   ##";
+	}
+	for($i=0;$i<$l;$i++)if($i==0||$i==79)echo "##";echo "</br>";
+	keret();
+	keret();
+	
 }
 echo "</center>";
 ?>
