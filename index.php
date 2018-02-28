bf71460ed1a4d13f72f5bb8d64ed1645bbb436ff
tomiborza <borzatamas00@gmail.com>
2018-02-28 10:08:31 +0100

    dupla sor#

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <?php
 echo "<center>";
 function greeting(var name){
+	var i;
 	for(i=0;i<79;i++)echo "#";echo "</br>";
-	for(i=0;i<79;i++)if(i==0||i==79)echo "#";echo "</br>";
-	if(name==NULL){
-		echo "# Happy easter my friend #";
-	}else{
-		echo "# Happy easter "+name+" #";
-	}
-	for(i=0;i<79;i++)if(i==0||i==79)echo "#";echo "</br>";
 	for(i=0;i<79;i++)echo "#";echo "</br>";
+	for(i=0;i<79;i++)if(i==0||i==79)echo "##";echo "</br>";
 
+	if(name==NULL){
+		echo "## Happy easter my friend ##";
+	}else{
+		echo "## Happy easter "+name+" ##";
+	}
+	for(i=0;i<79;i++)if(i==0||i==79)echo "##";echo "</br>";
+	for(i=0;i<79;i++)echo "#";echo "</br>";
+	for(i=0;i<79;i++)echo "#";echo "</br>";
 }
 echo "</center>";
 ?>
