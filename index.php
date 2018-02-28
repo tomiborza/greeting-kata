49ef2dcbae83b03a77d5ca7152d36b457159a29f
tomiborza <borzatamas00@gmail.com>
2018-02-28 10:13:43 +0100

    3 3 space

--- index.php
+++ index.php
@@ -7,9 +7,9 @@ function greeting(var name){
 	for(i=0;i<79;i++)if(i==0||i==79)echo "##";echo "</br>";
 
 	if(name==NULL){
-		echo "## Happy easter my friend ##";
+		echo "##   Happy easter my friend   ##";
 	}else{
-		echo "## Happy easter "+name+" ##";
+		echo "##   Happy easter "+name+"   ##";
 	}
 	for(i=0;i<79;i++)if(i==0||i==79)echo "##";echo "</br>";
 	for(i=0;i<79;i++)echo "#";echo "</br>";
