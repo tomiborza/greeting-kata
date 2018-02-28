74424268f5b5bf24051074b4c81dcbb949077753
tomiborza <borzatamas00@gmail.com>
2018-02-28 10:00:39 +0100

    csillag greeting vegere

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 function greeting(var name){
 	for(i=0;i<79;i++)echo "*";echo "</br>";
 	if(name==NULL){
-		echo "Happy easter my friend ";
+		echo "Happy easter my friend *";
 	}else{
-		echo "Happy easter "+name+" ";
+		echo "Happy easter "+name+" *";
 	}
 	for(i=0;i<79;i++)echo "*";echo "</br>";
 
