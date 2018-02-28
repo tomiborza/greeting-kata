69ebf43a55a1f50e58bfa80c3fccb8dba5515ef1
tomiborza <borzatamas00@gmail.com>
2018-02-28 10:06:27 +0100

    replace csillag

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 echo "<center>";
 function greeting(var name){
-	for(i=0;i<79;i++)echo "*";echo "</br>";
-	for(i=0;i<79;i++)if(i==0||i==79)echo "*";echo "</br>";
+	for(i=0;i<79;i++)echo "#";echo "</br>";
+	for(i=0;i<79;i++)if(i==0||i==79)echo "#";echo "</br>";
 	if(name==NULL){
-		echo "* Happy easter my friend *";
+		echo "# Happy easter my friend #";
 	}else{
-		echo "* Happy easter "+name+" *";
+		echo "# Happy easter "+name+" #";
 	}
-	for(i=0;i<79;i++)if(i==0||i==79)echo "*";echo "</br>";
-	for(i=0;i<79;i++)echo "*";echo "</br>";
+	for(i=0;i<79;i++)if(i==0||i==79)echo "#";echo "</br>";
+	for(i=0;i<79;i++)echo "#";echo "</br>";
 
 }
 echo "</center>";
