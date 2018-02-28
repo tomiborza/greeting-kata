e1c01af811895ee3e4f425c23fc8e0cf89fe611f
tomiborza <borzatamas00@gmail.com>
2018-02-28 09:59:28 +0100

    my friend

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 function greeting(var name){
 	for(i=0;i<79;i++)echo "*";echo "</br>";
-	echo "Happy easter "+name+" ";
+	if(name==NULL){
+		echo "Happy easter my friend ";
+	}else{
+		echo "Happy easter "+name+" ";
+	}
 	for(i=0;i<79;i++)echo "*";echo "</br>";
 
 }
